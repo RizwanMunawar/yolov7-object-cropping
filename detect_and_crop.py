8bbbb1edbce4a905963ea2e2df3ad7c0ae30cf93
Muhammad Rizwan Munawar <muhammadrizwanmunawar123@gmail.com>
2025-02-15 18:02:54 +0500

    Remove bbox plotting with --save-img

--- detect_and_crop.py
+++ detect_and_crop.py
@@ -139,7 +139,7 @@ def detect(save_img=False):
                         with open(txt_path + '.txt', 'a') as f:
                             f.write(('%g ' * len(line)).rstrip() % line + '\n')
 
-                    if save_img or view_img:  # Add bbox to image
+                    if view_img:  # Add bbox to image
                         label = f'{names[int(cls)]} {conf:.2f}'
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=1)
 
